76925f7e7b37e71b06836a12a4e0b4106a6bcadc
tdcline1 <tdcline3@gmail.com>
2025-04-09 16:50:18 -0400

    update allowed host

--- django_project/settings.py
+++ django_project/settings.py
@@ -29,7 +29,11 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", default=False)
 
-ALLOWED_HOSTS = ["warm-sands-53281.herokuapp.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = [
+    "warm-sands-53281-999c329f4afb.herokuapp.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
