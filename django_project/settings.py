927d57719e101e666bfb92596c10ec5bdd74d095
tdcline1 <tdcline3@gmail.com>
2025-04-09 15:32:30 -0400

    fixed bug in heroku.yml

--- django_project/settings.py
+++ django_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", default=False)
 
-ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["warm-sands-53281.herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
